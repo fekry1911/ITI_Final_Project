b9b4d62a3c395e008f72e966089ba3032987a460
Abdulrahman Fekry <abdofekryasda@gmail.com>
2025-12-20 23:15:29 +0200

    handle lines and stations and position of user

--- lib/featuers/near_stations/logic/get_nearby_stations_cubit.dart
+++ lib/featuers/near_stations/logic/get_nearby_stations_cubit.dart
@@ -4,6 +4,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:iti_moqaf/core/networking/api_result.dart';
 import 'package:meta/meta.dart';
 
+import '../../../core/utils/determine_position.dart';
 import '../data/model/near_stations_model.dart';
 import '../data/repo/get_nearby_stations.dart';
 
@@ -13,39 +14,11 @@ class GetNearbyStationsCubit extends Cubit<GetNearbyStationsState> {
   GetNearbyStationsRepo getNearbyStationsRepo;
   GetNearbyStationsCubit(this.getNearbyStationsRepo) : super(GetNearbyStationsInitial());
 
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      await Geolocator.openLocationSettings();
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
 
   Future<void> getNearbyStations() async {
     emit(GetNearbyStationsLoading());
     try {
-      Position position = await _determinePosition();
+      Position position = (await determinePosition()) as Position;
 
       var result = await getNearbyStationsRepo.fetNearpyStations(
           position.latitude, position.longitude, 10000);
--- lib/featuers/near_stations/screens/screen.dart
+++ lib/featuers/near_stations/screens/screen.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:iti_moqaf/core/di/di.dart';
 import 'package:iti_moqaf/featuers/near_stations/logic/get_nearby_stations_cubit.dart';
 import 'package:iti_moqaf/featuers/near_stations/screens/widgets/bottom_sheet.dart';
@@ -18,107 +17,85 @@ class NearStations extends StatefulWidget {
 }
 
 class _NearStationsState extends State<NearStations> {
+  late GetNearbyStationsCubit _cubit;
+
   @override
   void initState() {
     super.initState();
+    _cubit = getIt<GetNearbyStationsCubit>()..getNearbyStations();
 
-    // TODO: implement initState
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        barrierColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        builder: (context) {
+          return BlocProvider.value(
+            value: _cubit,
+            child: HomeBottomSheet(),
+          );
+        },
+      );
+    });
+  }
 
+  @override
+  void dispose() {
+    _cubit.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      lazy: false,
-      create: (context) =>
-      getIt<GetNearbyStationsCubit>()
-        ..getNearbyStations(),
-      child: BlocConsumer<GetNearbyStationsCubit, GetNearbyStationsState>(
-        listener: (context, state) {
-          if (state is GetNearbyStationsSuccess) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
+    return BlocProvider.value(
+      value: _cubit,
+      child: Stack(
+        children: [
+          ImageFiltered(
+            imageFilter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+            child: Image.network(
+              "https://developers.google.com/static/maps/documentation/tile/images/example-basemap-tile.png",
+              height: 1.sh,
+              fit: BoxFit.fill,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
               showModalBottomSheet(
                 context: context,
                 isScrollControlled: true,
                 barrierColor: Colors.transparent,
                 backgroundColor: Colors.transparent,
                 builder: (context) {
-                  return HomeBottomSheet(data: state.data, userPosition: state.userPosition);
+                  return BlocProvider.value(
+                    value: _cubit,
+                    child: HomeBottomSheet(),
+                  );
                 },
               );
-            });
-          }
-        },
-        builder: (context, state) {
-          return Stack(
-            children: [
-              ImageFiltered(
-                imageFilter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-
-                child: Image.network(
-                  "https://developers.google.com/static/maps/documentation/tile/images/example-basemap-tile.png",
-                  height: 1.sh,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              BlocBuilder<GetNearbyStationsCubit, GetNearbyStationsState>(
-                builder: (context, state) {
-
-
-                  if (state is GetNearbyStationsError) {
-                    return Center(
-                      child: Column(
-                        children: [
-                          Text(state.error),
-                          SizedBox(height: 20.h,),
-                          MaterialButton(onPressed: (){
-                            context.read<GetNearbyStationsCubit>().getNearbyStations();
-                          },child: Text("Retry"))
-                        ],
-                      ),
-                    );
-                  }
-                  if (state is GetNearbyStationsSuccess) {
-                  return GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        barrierColor: Colors.transparent,
-                        backgroundColor: Colors.transparent,
-                        builder: (context) {
-                          return HomeBottomSheet(data: state.data, userPosition: state.userPosition);
-                        },
-                      );
-                    },
-                    child: Header(),
-                  );
-                  }
-                  return Center(
-                      child: CircularProgressIndicator());
-                },
-              ),
-              Positioned(
-                top: 130.h,
-                right: 80.w,
-                child: MapInfoChip(
-                  icon: Icons.directions_bus_rounded,
-                  text: "12m",
-                  iconColor: Colors.blue,
-                ),
-              ),
-              Positioned(
-                top: 200.h,
-                left: 80.w,
-                child: MapInfoChip(
-                  icon: Icons.schedule_rounded,
-                  text: "Delayed",
-                  iconColor: Colors.orange,
-                ),
-              ),
-            ],
-          );
-        },
+            },
+            child: Header(),
+          ),
+          Positioned(
+            top: 130.h,
+            right: 80.w,
+            child: MapInfoChip(
+              icon: Icons.directions_bus_rounded,
+              text: "12m",
+              iconColor: Colors.blue,
+            ),
+          ),
+          Positioned(
+            top: 200.h,
+            left: 80.w,
+            child: MapInfoChip(
+              icon: Icons.schedule_rounded,
+              text: "Delayed",
+              iconColor: Colors.orange,
+            ),
+          ),
+        ],
       ),
     );
   }
